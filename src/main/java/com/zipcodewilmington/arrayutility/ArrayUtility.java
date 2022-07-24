3e46551d30e7d57d726dcdde30e9c75dec6b5e22
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-24 09:19:48 -0400

    collections is powerful

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,46 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    List<T> objectList;
+
+    public ArrayUtility(T[] inputArray) {
+        objectList = new ArrayList<>(Arrays.asList(inputArray));
+    }
+
+    public Object[] removeValue(T valueToRemove){
+        for(int i = 0; i < objectList.size(); i++){
+            objectList.remove(valueToRemove);
+        }
+
+        return objectList.toArray();
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate){
+        return Collections.frequency(objectList, valueToEvaluate);
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge){
+        List<T> arrToMerge = new ArrayList<>(Arrays.asList(arrayToMerge));
+        objectList.addAll(arrToMerge);
+
+        return objectList.stream().reduce(BinaryOperator.maxBy(((o1, o2)
+                -> Collections.frequency(objectList, o1) - Collections.frequency(objectList, o2)))).orElse(null);
+
+
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
+        List<T> arrToMerge = new ArrayList<>(Arrays.asList(arrayToMerge));
+        objectList.addAll(arrToMerge);
+
+        return Collections.frequency(objectList, valueToEvaluate);
+    }
 }
--- src/test/java/com/zipcodewilmington/arrayutility/RemoveValueTest.java
+++ src/test/java/com/zipcodewilmington/arrayutility/RemoveValueTest.java
@@ -1,6 +1,6 @@
 package com.zipcodewilmington.arrayutility;
 
-import com.zipcodewilmington.UnitTestingUtils;
+//import com.zipcodewilmington.UnitTestingUtils;
 import org.junit.Test;
 
 /**
@@ -18,7 +18,7 @@ public class RemoveValueTest {
 
 
         // When
-        Integer[] actual = arrayUtility.removeValue(valueToRemove);
+        Object[] actual = arrayUtility.removeValue(valueToRemove);
 
         // Then
         UnitTestingUtils.assertArrayEquality(expected, actual);
@@ -34,7 +34,7 @@ public class RemoveValueTest {
 
 
         // When
-        Long[] actual = arrayUtility.removeValue(valueToRemove);
+        Object[] actual = arrayUtility.removeValue(valueToRemove);
 
         // Then
         UnitTestingUtils.assertArrayEquality(expected, actual);
@@ -51,7 +51,7 @@ public class RemoveValueTest {
 
 
         // When
-        String[] actual = arrayUtility.removeValue(valueToRemove);
+        Object[] actual = arrayUtility.removeValue(valueToRemove);
 
         // Then
         UnitTestingUtils.assertArrayEquality(expected, actual);
